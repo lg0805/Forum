7c7f148128341a4d2f5b3c778857ccaeb7a53163
lg0805 <lg0805@qq.com>
2017-08-04 14:53:20 +0800

    modify user.php
    To improve the User class

--- lib/user.php
+++ lib/user.php
@@ -1,20 +1,34 @@
 <?php 
+
+
+
+
 require_once('db.php');
 require_once('common.php');
 require_once('functions.php');
 
 class User{
-	private $uid;
-	private $fields;
 
+	// Permission levers
+	const CREATE_FORUM = 2;
+	const MOVE_MESSAGE = 4;
+	const DELETE_MESSAGE = 8;
+	const DELETE_FORUM = 16;
+
+	private $uid;	//user id
+	private $fields;	// user record fields
+
+	// initalize a User object
 	public function __construct(){
 		$this->uid = null;
 		$this->fields = array('username' => '',
 								'password' => '',
 								'emailAddr' => '',
-								'isActive' => false);
+								'isActive' => false,
+								'permission' => 0);
 	}
 
+	// override magic method to retrieve properties
 	public function __get($field){
 		if ($field == 'userId') {
 			return $this ->uid;
@@ -23,6 +37,7 @@ class User{
 		}
 	}
 
+	// override magic method to set properties
 	public function __set($field, $value){
 
 		if (array_key_exists($field, $this->fields)) {
@@ -30,21 +45,24 @@ class User{
 		}
 	}
 
+	// return if username is valid format
 	public static function validateUsername($username){
 
 		return preg_match('/^[A-Z0-9]{2,20}$/i', $username);
 	}
 
+	// return if email address is valid format
 	public static function validateEmailAddr($email){
 
 		return filter_var($email, FILTER_VALIDATE_EMAIL);
 	}
 
+	// return an object populated based on the record's user id
 	public static function getById($user_id){
 
 		$user = new User();
 
-		$query =sprintf('SELECT USERNAME, PASSWORD, EMAIL_ADDR, IS_ACTIVE FROM %sUSER WHERE USER_ID = %d', DB_TBL_PREFIX, $user_id);
+		$query =sprintf('SELECT USERNAME, PASSWORD, EMAIL_ADDR, IS_ACTIVE, PERMISSION FROM %sUSER WHERE USER_ID = %d', DB_TBL_PREFIX, $user_id);
 
 		$result = mysql_query($query, $GLOBALS['DB']);
 
@@ -55,17 +73,19 @@ class User{
 			$user->password = $row['PASSWORD'];
 			$user->emailAddr = $row['EMAIL_ADDR'];
 			$user->isActive = $row['IS_ACTIVE'];
+			$user->permission = $row['PERMISSION'];
 			$user->uid = $user_id;
 		}
 		mysql_free_result($result);
 		return $user;
 	}
 
+	// return an object populated based on the record's username
 	public static function getByUsername($username){
 
 		$user = new User();
 
-		$query = "SELECT USER_ID, PASSWORD, EMAIL_ADDR, IS_ACTIVE FROM WROX_USER WHERE USERNAME = '$username'";
+		$query = "SELECT USER_ID, PASSWORD, EMAIL_ADDR, IS_ACTIVE, PERMISSION FROM WROX_USER WHERE USERNAME = '$username'";
 
 		$result = mysql_query($query, $GLOBALS['DB']);
 
@@ -75,23 +95,24 @@ class User{
 			$user->password = $row['PASSWORD'];
 			$user->emailAddr = $row['EMAIL_ADDR'];
 			$user->isActive = $row['IS_ACTIVE'];
+			$user->permission = $row['PERMISSION'];
 			$user->uid = $row['USER_ID'];
 		}
 		mysql_free_result($result);
 		return $user;
 	}
 
-
+	// save the record to the database
 	public function save(){
 
 		if ($this->uid) {
 			// 更新记录
-			$query = sprintf('UPDATE %sUSER SET USERNAME = "%s", PASSWORD = "%s", EMAIL_ADDR = "%s", IS_ACTIVE = %d WHERE USER_ID=%d', DB_TBL_PREFIX, $this->username, $this->password, $this->emailAddr, $this->isActive, $this->userId);
+			$query = sprintf('UPDATE %sUSER SET USERNAME = "%s", PASSWORD = "%s", EMAIL_ADDR = "%s", IS_ACTIVE = %d, PERMISSION = %d WHERE USER_ID=%d', DB_TBL_PREFIX, $this->username, $this->password, $this->emailAddr, $this->isActive, $this->permission, $this->uid);
 			
 			return mysql_query($query, $GLOBALS['DB']);
 		} else {
 
-			$query = "INSERT INTO WROX_USER(USERNAME, PASSWORD, EMAIL_ADDR, IS_ACTIVE) VALUES('$this->username', '$this->password','$this->emailAddr',0)";
+			$query = "INSERT INTO WROX_USER(USERNAME, PASSWORD, EMAIL_ADDR, IS_ACTIVE, PERMISSION) VALUES('$this->username', '$this->password','$this->emailAddr','$this->isActive, $this->permission')";
 			// echo $query;
 			// exit();
 
@@ -103,6 +124,44 @@ class User{
 			 }
 		}
 	}
+
+	// set the records as inactive and return an activation token
+	public function setPending(){
+		$this->isActive = false;
+		$this->save(); // make sure the record is saved
+
+		$token = random_text(5);
+		$query = sprintf('INSERT INTO %sPENDING(USER_ID, TOKEN) VALUES(%d, "%s")', DB_TBL_PRIFIX, $this->uid, $token);
+		return (mysql_query($query, $GLOBALS['DB'])) ? $token : false;
+	}
+
+
+	// clear the user's pending status and set the record as active
+	public function clearPending($token){
+
+		$query = sprintf('SELECT TOKEN FROM %sPENDING WHERE USER_ID=%d AND TOKEN = "%s"', DB_TBL_PRIFIX, $this->uid, 
+			mysql_real_escape_string($token, $GLOBALS['DB']));
+
+		$result = mysql_query($query, $GLOBALS['DB']);
+
+		if (!mysql_num_rows($result)) {
+			mysql_free_result($result);
+			return false;
+		} else {
+			mysql_free_result($result);
+			$query = sprintf('DELETE FORM %sPENDING WHERE USER_ID = %d AND TOKEN = "%s"', DB_TBL_PRIFIX, $this->uid, 
+				mysql_real_escape_string($token, $GLOBALS['DB']));
+
+			if (!mysql_query($query, $GLOBALS['DB'])) {
+				return false;
+			} else {
+				$this->isActive = true;
+				return $this->save();
+			}
+
+		}
+	}
+
 
 	public function setInactive(){
 
