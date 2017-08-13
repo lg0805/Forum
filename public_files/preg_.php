5004be0b68e5172ba58308251b3a7f4326b7abb4
lg0805 <lg0805@qq.com>
2017-08-13 19:27:25 +0800

    submit preg_match

--- public_files/preg_.php
+++ public_files/preg_.php
@@ -1,6 +1,6 @@
 <?php 
-
-	$pattern = '/<a.*?(?:|\\t|\\r|\\n)?href=[\'"]?(.+?)[\'"]?(?:(?:|\\t|\\n)+.*?)?>(.+?)<\/a.*?>/sim';
+	error_reporting(E_ALL);
+	/*$pattern = '/<a.*?(?:|\\t|\\r|\\n)?href=[\'"]?(.+?)[\'"]?(?:(?:|\\t|\\n)+.*?)?>(.+?)<\/a.*?>/sim';
 	$subject = "<a href='www.baidu.com'>百度</a>";
 
 	if (preg_match($pattern, $subject, $result)) {
@@ -9,7 +9,8 @@
 		echo "没有找到有效的标签";
 	}
 
-	print_r($result);
+	print_r($result);*/
+
 	// 匹配成功Array
 	// (
  	//    [0] => <a href='www.baidu.com'>百度</a>
@@ -33,16 +34,25 @@
 	// $pattern2 = '/1.+?99/';	// 结果：1999
 	// $pattern2 = "/1.?99/";	// 结果：1999
 	
-	$pattern2 = "/\bfor\b/";	// 结果：yes
+	/*
+	 * --参考资料--
+	 * preg_match_all使用心得分享_php技巧_脚本之家
+     * http://www.jb51.net/article/46435.htm
+     */
+    
+    // 使用反向引用时，如果使用单引可表示为'\1',如果使用使用双引号，则表示为"\\1"
+	$pattern2 = '#\[([a-z]+)=?(.*?)\](.*?)\[/\1\]#';	 
+	// $pattern2 = "#\[([a-z]+)=?(.*?)\](.*?)\[/[a-z]+\]#";	
+	// $pattern2 = "|(.*?)|";	// 结果：yes
 	
-	$subject2 = "for fore";
+	$subject2 = "[color=green]test[/color]";
 
-	if (preg_match($pattern2, $subject2, $result2)) {
+	if (preg_match($pattern2, $subject2, $part, PREG_OFFSET_CAPTURE)) {
 		echo "匹配成功";
 	} else {
 		echo "没有找到有效的标签";
 	}
 
-	var_dump($result2);
+	var_dump($part);
 
 ?>
